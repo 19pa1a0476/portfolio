6d11b91ea4480e8d7aec814f521209268b1d06bd
19pa1a0476 <19pa1a0476@vishnu.edu.in>
2025-08-29 22:55:14 +0530

    added computer 3d code for mobile support

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   // ✅ Load model from public folder
-  const computer = useGLTF("/desktop_pc/scene.gltf");
+  const computer = useGLTF("/desktop_pc/scene.glb");
 
   return (
     <mesh>
